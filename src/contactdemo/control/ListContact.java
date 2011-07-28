cc578ce53925ce8e0618c76db35ded8eafdfdd08
Toddy Zhou <zhoutaomtv@gmail.com>
2011-07-28 19:54:56 +0800

    add configuration files

--- src/contactdemo/control/ListContact.java
+++ src/contactdemo/control/ListContact.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import contactdemo.entity.Contact;
 import contactdemo.service.ContactServiceImpl;
 import contactdemo.service.IContactService;
--- src/contactdemo/control/SaveOrUpdateContact.java
+++ src/contactdemo/control/SaveOrUpdateContact.java
@@ -2,6 +2,10 @@ package contactdemo.control;
 
 import java.io.IOException;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import com.google.appengine.api.datastore.Text;
 
 import contactdemo.entity.Contact;
